bfe1623219f057ff92be48b22fdea21d918d465d
Lealuski <77172302+Lealuski@users.noreply.github.com>
2024-11-09 20:42:20 -0500

    Nuevas APIs para crear y eliminar registros de banks de manera masiva
    Las urls nuevas son:
    - postManyBanks (POST): {{HOST}}/banks/bulk
    - deleteAllBank (DELETE): {{HOST}}/banks
    
    El json de postManyBanks es un arreglo de CreateBankDto.
    
    El archivo banks.json es una modificacion de fraudes.json, pero esta formateado de manera que se use en el createManyBanks, y asi crear todos los registros de bancos a informar.

--- src/banks/banks.controller.ts
+++ src/banks/banks.controller.ts
@@ -9,51 +9,80 @@ export class BanksController {
 
     @Post()
     create(@Body() createBankDto : CreateBankDto, @Res() response){
-        this.banksService.createBank(createBankDto).then( bank => {
-            response.status(HttpStatus.CREATED).json(bank);
-        }).catch(() => {
-            response.status(HttpStatus.FORBIDDEN).json(
-                {mensaje: 'Error in the creation of the bank'});
-        });
+        this.banksService.createBank(createBankDto)
+            .then( bank => {
+                response.status(HttpStatus.CREATED).json(bank);
+            }).catch(() => {
+                response.status(HttpStatus.FORBIDDEN).json(
+                    {mensaje: 'Error in the creation of the bank'});
+            });
+    }
+
+    @Post('bulk')
+    createMany(@Body() createBanksDto: CreateBankDto[], @Res() response) {
+        this.banksService.createBanks(createBanksDto)
+            .then(banks => {
+                response.status(HttpStatus.CREATED).json(banks);
+            })
+            .catch(() => {
+                response.status(HttpStatus.FORBIDDEN).json(
+                    { mensaje: 'Error in the creation of banks' }
+                );
+            });
     }
 
     @Get()
     getAll(@Res() response){
-        this.banksService.getAll().then( banksList => {
-            response.status(HttpStatus.OK).json(banksList);
-        }).catch( () => {
-            response.status(HttpStatus.FORBIDDEN).json(
-                {mensaje: 'Error in the getting of the banks'});
-        });
+        this.banksService.getAll()
+            .then( banksList => {
+                response.status(HttpStatus.OK).json(banksList);
+            }).catch( () => {
+                response.status(HttpStatus.FORBIDDEN).json(
+                    {mensaje: 'Error in the getting of the banks'});
+            });
     }
 
     @Get(':id')
     getById(@Res() response,@Param('id') idBank){
-        this.banksService.getById(idBank).then( bank => {
-            response.status(HttpStatus.OK).json(bank);
-        }).catch( () => {
-            response.status(HttpStatus.FORBIDDEN).json(
-                {mensaje: 'Error in the getting of the bank by id'});
-        });
+        this.banksService.getById(idBank)
+            .then( bank => {
+                response.status(HttpStatus.OK).json(bank);
+            }).catch( () => {
+                response.status(HttpStatus.FORBIDDEN).json(
+                    {mensaje: 'Error in the getting of the bank by id'});
+            });
     }
 
     @Put(':id')
     update(@Body() updateBankDto: CreateBankDto, @Res() response, @Param('id') idBank : number){
-        this.banksService.updateBank(idBank, updateBankDto).then(bankUpdated => {
-            response.status(HttpStatus.OK).json(bankUpdated);
-        }).catch(() => {
-            response.status(HttpStatus.FORBIDDEN).json(
-                {mensaje: 'Error in the modification of the bank'});
-        });
+        this.banksService.updateBank(idBank, updateBankDto)
+            .then(bankUpdated => {
+                response.status(HttpStatus.OK).json(bankUpdated);
+            }).catch(() => {
+                response.status(HttpStatus.FORBIDDEN).json(
+                    {mensaje: 'Error in the modification of the bank'});
+            });
     }
 
     @Delete(':id')
     delete(@Res() response, @Param('id') idBank : number){
-        this.banksService.deleteBank(idBank).then(res => {
-            response.status(HttpStatus.OK).json(res);
-        }).catch(() => {
-            response.status(HttpStatus.FORBIDDEN).json(
-                {mensaje: 'Error in the elimination of the bank'});
-        });
+        this.banksService.deleteBank(idBank)
+            .then(res => {
+                response.status(HttpStatus.OK).json(res);
+            }).catch(() => {
+                response.status(HttpStatus.FORBIDDEN).json(
+                    {mensaje: 'Error in the elimination of the bank'});
+            });
+    }
+
+    @Delete()
+    deleteAll(@Res() response){
+        this.banksService.deleteBanks()
+            .then(res => {
+                response.status(HttpStatus.OK).json(res);
+            }).catch(() => {
+                response.status(HttpStatus.FORBIDDEN).json(
+                    {mensaje: 'Error in the elimination of all the banks'});
+            });
     }
 }
--- src/banks/banks.service.ts
+++ src/banks/banks.service.ts
@@ -12,15 +12,15 @@ export class BanksService {
         private readonly bankRepository: Repository<Bank>,
     ) {}
 
-    async getAll(): Promise<Bank[]>{
+    async getAll(): Promise<Bank[]> {
         return await this.bankRepository.find();
     }
 
-    async getById(idBank: number): Promise<Bank>{
+    async getById(idBank: number): Promise<Bank> {
         return await this.bankRepository.findOneBy({id:idBank});
     }
 
-    async createBank(newBank: CreateBankDto): Promise<Bank>{
+    async createBank(newBank: CreateBankDto): Promise<Bank> {
         const newb = new Bank();
         newb.name = newBank.name;
         newb.contact_number = newBank.contact_number;
@@ -30,7 +30,7 @@ export class BanksService {
         return this.bankRepository.save(newb);
     }
 
-    async updateBank(idBank: number, oldBank: CreateBankDto): Promise<Bank>{
+    async updateBank(idBank: number, oldBank: CreateBankDto): Promise<Bank> {
         const bankUpdate: Bank = await this.bankRepository.findOneBy({id:idBank});
         bankUpdate.name = oldBank.name;
         bankUpdate.contact_number = oldBank.contact_number;
@@ -40,7 +40,25 @@ export class BanksService {
         return this.bankRepository.save(bankUpdate);
     }
 
-    async deleteBank(idBank: number): Promise<any>{
+    async deleteBank(idBank: number): Promise<any> {
         return await this.bankRepository.delete(idBank);
     }
+
+    async createBanks(newBanks: CreateBankDto[]): Promise<Bank[]> {
+        const banksToSave = newBanks.map(newBank => {
+            const bank = new Bank();
+            bank.name = newBank.name;
+            bank.contact_number = newBank.contact_number;
+            bank.branches = newBank.branches;
+            bank.link = newBank.link;
+            bank.email = newBank.email;
+            return bank;
+        });
+        return this.bankRepository.save(banksToSave);
+    }
+    
+    async deleteBanks(): Promise<void> {
+        await this.bankRepository.clear();
+    }
+    
 }
